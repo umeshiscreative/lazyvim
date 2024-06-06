ef812f743b6f3490d6879118a49e90937933aedb
Umesh Kumar Verma <umesh-kumar.verma@hpe.com>
2024-06-06 10:28:16 +0530

    Language support added

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,16 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.editor.mini-move" },
+    { import = "lazyvim.plugins.extras.linting.eslint" }, -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.rust" }, --Install this rust-analyzer also for rustIDE support =>
+    --rustup component add rust-analyzer
     { import = "plugins" },
   },
   defaults = {
