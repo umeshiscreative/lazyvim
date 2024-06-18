8c4440f1dddc85d3daefcf14cf388219bd7251d5
Umesh Kumar Verma <umesh-kumar.verma@hpe.com>
2024-06-18 16:31:03 +0530

    Added Java Support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.angular" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "plugins" },
   },
   defaults = {
